5d947f1d285fb099030f1c6e3b581b3bdbd6b4c0
Svetlana Morozova <svetlanochka@yandex.ru>
2023-01-11 00:28:16 +0300

    Создание книжной библиотеки для ДЗ №13

---
+++ src/pro/sky/java/course1/lesson13/Author.java
@@ -0,0 +1,23 @@
+package pro.sky.java.course1.lesson13;
+
+public class Author {
+    private String name;
+    private String lastName;
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.name + " " + this.lastName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
